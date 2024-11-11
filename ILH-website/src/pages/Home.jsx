a5974708fcae8b25668c42470ae74dafe152865a
it-IntegratedLifetechHub <it@integratedlifetechhub.com>
2024-11-11 17:22:29 +0530

    about-us section | our-business section completed

--- ILH-website/src/pages/Home.jsx
+++ ILH-website/src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import aboutus from "../assets/aboutus.png";
+import agriculture from "../assets/business1.png";
+import advisory from "../assets/business2.png";
+import finance from "../assets/business3.png";
+import food from "../assets/business3.png";
+import warehousing from "../assets/business3.png";
 
 const Home = () => {
   return (
@@ -24,7 +29,49 @@ const Home = () => {
 
       <div className="about-section">
         <div className="bgbox">
-          <img src={aboutus} alt="" srcset="" />
+          <img src={aboutus} alt="About Us Image" className="about-image" />
+          <div className="text-content">
+            <h1 className="about-title">Introduction</h1>
+            <p className="about-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              porro quibusdam reiciendis modi sed impedit recusandae in tenetur
+              animi maiores laudantium libero dolore ratione autem. Totam est
+              earum excepturi placeat. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Itaque, porro quibusdam reiciendis modi sed
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              porro quibusdam reiciendis modi sed impedit recusandae in tenetur
+              animi maiores laudantium libero dolore ratione autem. Totam est
+            </p>
+            <div>
+              <button className="about-button">Know More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="business-section">
+        <div className="business-bgbox">
+          <h1 className="business-subtitle">
+            Integrated Lifetech Hub India Pvt. Ltd.
+          </h1>
+          <h1 className="business-title">Our Division</h1>
+          <span className="bottom-dot">•••</span>
+          <div className="business-cards">
+            {[
+              { title: "Agriculture", imgSrc: agriculture },
+              { title: "Advisory", imgSrc: advisory },
+              { title: "Finance", imgSrc: finance },
+              { title: "Food Processing", imgSrc: food },
+              { title: "Warehousing", imgSrc: warehousing },
+            ].map((card, index) => (
+              <div className="bcard" key={index}>
+                <h1>{card.title}</h1>
+                <img src={card.imgSrc} alt={card.title} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
