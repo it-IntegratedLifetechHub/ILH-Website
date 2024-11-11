dba1b4219a061e257b184234686314c3c1f94bf6
it-IntegratedLifetechHub <it@integratedlifetechhub.com>
2024-11-11 14:30:39 +0530

    hero section completed with title and subtitle

--- ILH-website/src/pages/Home.jsx
+++ ILH-website/src/pages/Home.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import aboutus from "../assets/aboutus.png";
 
 const Home = () => {
   return (
     <>
       <div className="homepage" id="home">
         <div className="hero-content">
+          <h1 className="hero-title">
+            Integrated <br />
+            Lifetech Hub <br />
+            India Pvt. Ltd.
+          </h1>
+
+          <hr className="hero-divider" />
+
+          <p className="hero-subtitle">Bhu-Samriddhi, Jeevan-Unnati</p>
+
           <div className="hero-image">
-            {/* The background image will be styled via CSS */}
+            {/* Background image styled via CSS */}
           </div>
+        </div>
+      </div>
+
+      <div className="about-section">
+        <div className="bgbox">
+          <img src={aboutus} alt="" srcset="" />
         </div>
       </div>
     </>
