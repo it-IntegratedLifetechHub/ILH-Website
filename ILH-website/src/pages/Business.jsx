83aed4a56e0ed9a518d84a4d2de3140bfa62fcbe
it-IntegratedLifetechHub <it@integratedlifetechhub.com>
2024-11-22 13:54:05 +0530

    our divisions business listings

--- ILH-website/src/pages/Business.jsx
+++ ILH-website/src/pages/Business.jsx
@@ -1,25 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./business.css";
 import allbusiness from "../assets/allbusiness.png";
+import aboutus from "../assets/aboutus.png";
+import { Link } from "react-router-dom";
 
 const Business = () => {
+  const [sections, setSections] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from JSON file
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => setSections(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
   return (
-    <div className="business-page">
-      <img
-        src={allbusiness}
-        alt="Our Business Overview"
-        className="ourbusiness-img"
-      />
-      <div className="business-text">
-        <h1 className="business-page-title">Our Business</h1>
-        <p className="business-page-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum modi
-          incidunt tempora assumenda est minima mollitia unde! Mollitia cumque
-          exercitationem, amet adipisci ullam nemo culpa, placeat similique
-          tempora facere eius.
-        </p>
+    <>
+      <div className="business-page">
+        <img
+          src={allbusiness}
+          alt="Our Business Overview"
+          className="ourbusiness-img"
+        />
+        <div className="business-text">
+          <h1 className="business-page-title">Our Business</h1>
+          <p className="business-page-description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum modi
+            incidunt tempora assumenda est minima mollitia unde! Mollitia cumque
+            exercitationem, amet adipisci ullam nemo culpa, placeat similique
+            tempora facere eius.
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="about-container">
+        {sections.map((section, index) => (
+          <div
+            className={`about-section ${index % 2 !== 0 ? "row-reversed" : ""}`}
+            key={section.id}
+          >
+            <div className="bgbox">
+              <img
+                src={section.image}
+                alt={`${section.title} Image`}
+                className="about-image"
+              />
+              <div className="text-content">
+                <h1 className="about-title">{section.title}</h1>
+                <p className="about-description">{section.description}</p>
+                <div>
+                  <Link to={section.link}>
+                    <button className="about-button">Know More</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
